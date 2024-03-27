e240b03bae38a284cc6c1849c787c154ea72e903
hablu <131282586+hablu@users.noreply.github.com>
2024-03-27 09:53:03 +0600

    Creating API for single student registration.

---
+++ src/app/api/students/route.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server"
+
+export const POST = async (req, res) => {
+  try {
+    const prisma = new PrismaClient();
+    const result = await prisma.user.create({
+      data: {
+        first_name: "Rashed",
+        last_name: "Akash",
+        roll: 22,
+        age: 24,
+        grade: "A",
+        courses:"Accounting"
+      }
+    })
+    return NextResponse.json(result);  
+  } catch (error) {
+   return NextResponse.json(error) 
+  }
+}
