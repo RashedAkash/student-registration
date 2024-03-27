ab7507dda5d708ea0fef9865fb7445e0964d7c51
hablu <131282586+hablu@users.noreply.github.com>
2024-03-27 10:22:02 +0600

    API needs to be created to update student information.

---
+++ src/app/api/updateStudent/route.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export const PUT = async (req, res) => {
+    
+    const { searchParams } = new URL(req.url);
+    const id = parseInt(searchParams.get("id"));
+    const prisma = new PrismaClient();
+    const result = await prisma.user.update({
+      where: { id: id },
+        data: {
+    first_name: "Virat",
+    last_name: "Kohli",
+    roll: 40,
+    age: 18,
+    grade: "A",
+    courses: "Cricket"
+  },
+    });
+    return NextResponse.json(result);
+  // try {
+  
+  // } catch (error) {
+  //   return NextResponse.json(error);
+  // }
+};
